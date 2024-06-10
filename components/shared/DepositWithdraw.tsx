c56084abbc28fe170f6e404f1733d7c6d76f9180
asseph <davidkano.dk@gmail.com>
2024-06-10 14:34:39 +0700

    deposit withdraw

--- components/shared/DepositWithdraw.tsx
+++ components/shared/DepositWithdraw.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export const DepositWithdraw = ({ deposit, withdraw }: any) => {
+  const [inputValue, setInputValue] = React.useState(0)
+
   return (
     <>
       <div className="w-full flex flex-col items-center justify-center gap-2 p-8 bg-card-bg rounded-lg lg:rounded-xl text-center border border-white border-opacity-10">
         <span className='text-xl font-black -mt-2'>Deposit/Withdraw</span>
         <div className="max-w-md mx-auto">
-          <input type="text" placeholder="Type here" className="input input-bordered w-full bg-transparent " />
+          <input type="number" placeholder="0" className="input input-bordered w-full bg-transparent " />
           <div className="w-full flex items-center justify-between gap-4 mt-4">
             <button className='mainCTA'>Deposit</button>
             <button className='secondaryCTA'>Withdraw</button>
